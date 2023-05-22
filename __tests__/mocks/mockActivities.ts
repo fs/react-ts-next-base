921748b725e92babd38e77a57053ce0999915a6f
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2023-05-22 13:41:37 +0300

    fix mocks

--- __tests__/mocks/mockActivities.ts
+++ __tests__/mocks/mockActivities.ts
@@ -1,22 +1,11 @@
-import { ActivityEvent } from 'graphql/types';
 import { mockPageInfo } from './mockPageInfo';
 
 export const mockActivities = [
   {
-    __typename: 'Activity' as const,
+    __typename: 'PublicActivity' as const,
     body: 'text',
-    createdAt: '20-10-2022',
-    event: ActivityEvent.UserLoggedIn,
     id: '1',
     title: 'title',
-    user: {
-      __typename: 'User' as const,
-      avatarUrl: null,
-      email: 'test@test.com',
-      firstName: 'firstName',
-      id: '1',
-      lastName: 'lastName',
-    },
   },
 ];
 
